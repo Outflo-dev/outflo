9156f278db9b3980b7adc8a3bcd02a4980241152
Outflo <outflo.xyz@gmail.com>
2026-05-06 10:09:49 -0400

    Rename middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -17,7 +17,7 @@ import { NextResponse, type NextRequest } from "next/server";
 /* ------------------------------
    Middleware
 -------------------------------- */
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     let response = NextResponse.next({
         request,
     });
